56bef43bcc310f85cc34afe9ae875c50d81cf9af
minh <Lminhphu0304@gmail.com>
2020-05-20 08:57:38 +1200

    Add more search fields in search component

--- frontend/src/Search.jsx
+++ frontend/src/Search.jsx
@@ -1,34 +1,94 @@
 import React from 'react';
-import { TextField, Button } from '@material-ui/core';
+import { TextField, Button, InputLabel, MenuItem, Select } from '@material-ui/core';
 
 import './search.css';
 
-export class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { Search: '' };
-  }
-  myChangeHandler = (event) => {
-    this.setState({ Search: event.target.value });
-  }
-  render() {
-    return (
-      <form>
-        <input
-          type='text'
-          onChange={this.myChangeHandler}
-        />
-      </form>
-    );
-  }
-}
+const searchField = [
+  'Method',
+  'Methodlogy',
+  'Benefit',
+  'Participants',
+]
 
-export function SearchCom (props) {
+const operator = [
+  'And',
+  'Or',
+  'And not',
+  'Or not',
+]
+
+// Warning: this is a place holder value only
+const value = [
+  'Performance',
+  'Improve code quality',  
+]
+
+export function Search(props) {
   return (
     <div className="search__container">
-      <form noValidate autoComplete="off">
-        <label>Description <TextField id="standard-basic" label="Description" variant="outlined" /></label>
-        <Button>Search</Button>
+      <form class="search__form" noValidate autoComplete="off">
+        <InputLabel className="description__search_field">
+          Search for: <TextField className="search__Description_input" label="Description" variant="outlined" />
+        </InputLabel>
+        <InputLabel className="description__search_field">Date: <TextField
+          label="From"
+          type="date"
+          defaultValue=""
+          variant="outlined"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+          <TextField
+            label="To"
+            type="date"
+            defaultValue=""
+            variant="outlined"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </InputLabel>
+        <InputLabel className="description__search_field">
+          If
+          <TextField
+            className="search__field__Select"
+            select
+            label="Field"
+            variant="outlined"
+          >
+            {searchField.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            className="search__field__Select"
+            select
+            label="Operator"
+            variant="outlined"
+          >
+            {operator.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            className="search__field__Select"
+            select
+            label="Value"
+            variant="outlined"
+          >
+            {value.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option}
+              </MenuItem>
+            ))}
+          </TextField>
+        </InputLabel>
+        <Button className="btn__primary" variant="contained">Search</Button>
       </form>
     </div>
   );
