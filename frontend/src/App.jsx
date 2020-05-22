e363f7c841d6ee6629b0de54eb3862bcf9a7785d
minh <Lminhphu0304@gmail.com>
2020-05-23 09:29:30 +1200

    Add loading idicator in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,37 @@
 import React, { useState } from 'react';
 
-import seer from './seer.svg';
+import SeerLogo from './seer.svg';
 import './App.css';
-import {Search} from './components/search';
+import { Theme } from './theme'
+import { Search } from './components/search';
 import { ArticleList } from './components/articleList';
+import { CircularLoading } from './components/progress'
 import { convertSearchValueToURLParam } from './utils'
 
 function App() {
-
   const [articles, setArticle] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   const submitSearch = async (searchValue) => {
+    setIsLoading(true)
     const queryParam = convertSearchValueToURLParam(searchValue)
     const res = await fetch(`/search?${queryParam}`)
     const data = await res.json()
+    setIsLoading(false)
     setArticle(data)
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={seer} alt="Seer logo" className="logo"/>
-      </header>
-      <Search submitSearch={submitSearch}/>
-      <ArticleList articles={articles}/>
-    </div>
-
+    <Theme>
+      <div className="App">
+        <header className="App-header">
+          <img src={SeerLogo} alt="Seer logo" className="logo" />
+        </header>
+        <Search submitSearch={submitSearch} />
+        { isLoading && <CircularLoading />}
+        <ArticleList articles={articles} />
+      </div>
+    </Theme>
   );
 }
 
