76b4bc35dea53512306eb6adac1b96a96ff18424
minh <Lminhphu0304@gmail.com>
2020-06-01 17:23:01 +1200

    Enhance: user model

--- model/user.js
+++ model/user.js
@@ -4,18 +4,31 @@ const user = new Schema({
   email: String, // used for log in
   password: String,
   name: String,
-  role: [String], // ["admin", "analyzer", "submitter", "searcher"]
-  submittedArticles: [{
-    type: Schema.Types.ObjectId,
-    ref: 'articles',
-  }],
-  reviewingArticles: [
-    {
+  role: {
+    type: [String],
+    enum: ['admin', 'analyzer', 'submitter', 'searcher'],
+    default: ['searcher'],
+  }, // ["admin", "analyzer", "submitter", "searcher"]
+  submittedArticles: {
+    type: [{
       type: Schema.Types.ObjectId,
       ref: 'articles',
-    },
-  ], // Could be empty
-  savedSearchs: [String],
+    }],
+    default: [],
+  },
+  reviewingArticles: {
+    type: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'articles',
+      },
+    ],
+    default: [],
+  }, // Could be empty
+  savedSearchs: {
+    type: [String],
+    default: [],
+  },
 });
 
 const userModel = model('user', user);
