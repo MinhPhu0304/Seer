e30bee6bfe1209a14fd8d96f1aaed69bc960e85c
minh <Lminhphu0304@gmail.com>
2020-06-01 17:01:38 +1200

    Feat: change Register input text box

--- frontend/src/components/LoginForm/index.jsx
+++ frontend/src/components/LoginForm/index.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import { Button } from '@material-ui/core'
+import React, { useState } from "react";
+import { Button, TextField } from '@material-ui/core'
 
 import Login from './ValidatedLoginForm'
 
 export default function LoginForm() {
-  const [signUp, setShowSignUp] = React.useState(false);
-  const [emailAddress, setUserEmail] = React.useState("");
-  const [userGender, setUserGender] = React.useState("");
-  const [mobileNumber, setUserMobile] = React.useState("");
+  const [signUp, setShowSignUp] = useState(false);
+  const [emailAddress, setUserEmail] = useState("");
+  const [password, setPassword] = useState('');
+  const [userGender, setUserGender] = useState("");
+  const [mobileNumber, setUserMobile] = useState("");
 
   const clickHandler = event => {
     setShowSignUp(true);
@@ -28,6 +29,9 @@ export default function LoginForm() {
       case "Mobile":
         setUserMobile(event.target.value);
         break;
+      case "Password":
+        setPassword(event.target.value)
+        break;
       default:
         break;
     }
@@ -44,76 +48,78 @@ export default function LoginForm() {
       </p>}
       {signUp === true && (
         <div>
-          <label className="form-label">Email: </label>
-          <div className="form-content">
-            <input
-              type="email"
-              placeholder="Email Address"
-              name="Email"
-              value={emailAddress}
-              onChange={changeHandler}
-              required
-            />
-          </div>
-          <br />
-          <label className="form-label">Password: </label>
-          <div className="form-content">
-            <input
-              type="password"
-              placeholder="Enter Password"
-              name="Password"
-              onChange={changeHandler}
-              required
-            />
-          </div>
-          <br />
+          <form>
+            <div className="form-content">
+              <TextField
+                type="email"
+                label="Email"
+                name="Email"
+                variant="outlined"
+                value={emailAddress}
+                onChange={changeHandler}
+                required
+              />
+            </div>
+            <br />
+            <div className="form-content">
+              <TextField
+                variant="outlined"
+                type="password"
+                label="Password"
+                name="Password"
+                value={password}
+                onChange={changeHandler}
+                required
+              />
+            </div>
+            <br />
+            <div className="form-content">
+              <TextField
+                variant="outlined"
+                label="Re-enter password"
+                type="password"
+                placeholder="Re-enter Password"
+                name="Password1"
+                onChange={changeHandler}
+                required
+              />
+            </div>
+            <br />
+            <label className="form-label">Gender: </label>
+            <div className="form-content">
+              <select name="Gender" value={userGender} onChange={changeHandler}>
+                <option>Male</option>
+                <option>Female</option>
+              </select>
+            </div>
 
-          <label className="form-label">Password: </label>
-          <div className="form-content">
-            <input
-              type="password"
-              placeholder="Re-enter Password"
-              name="Password1"
-              onChange={changeHandler}
-              required
-            />
-          </div>
+            <br />
+            <label className="form-label">Mobile: </label>
+            <div className="form-content">
+              <TextField
+                type="tel"
+                variant="outlined"
+                label="Phone number"
+                name="Mobile"
+                value={mobileNumber}
+                onChange={changeHandler}
+                required
+              />
+            </div>
 
-          <br />
-
-
-          <label className="form-label">Gender: </label>
-          <div className="form-content">
-            <select name="Gender" value={userGender} onChange={changeHandler}>
-              <option>Male</option>
-              <option>Female</option>
-            </select>
-          </div>
-
-          <br />
-          <label className="form-label">Mobile: </label>
-          <div className="form-content">
-            <input
-              type="tel"
-              placeholder="Mobile number"
-              name="Mobile"
-              value={mobileNumber}
-              onChange={changeHandler}
-              required
-            />
-          </div>
-
-          <br />
-          <center>
-            <Button color="primary" onClick={clickHandler}>Register</Button>
-            <p>
-              Want to log in instead ?
+            <br />
+            <center>
+              <Button color="primary" type="submit" onClick={clickHandler}>Register</Button>
+              <p>
+                Want to log in instead ?
               <Button color="primary" onClick={clickHandlerLogin}>
-                Login
+                  Login
               </Button>
-            </p>
-          </center>
+              </p>
+            </center>
+          </form>
         </div>
+
       )}
     </div>
   );
