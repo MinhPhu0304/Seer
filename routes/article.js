a644ec7039ee9410da4b94f980f5b002f97b5768
minh <Lminhphu0304@gmail.com>
2020-06-02 21:16:00 +1200

    Feature: add api endpoint get submitted article

--- routes/article.js
+++ routes/article.js
@@ -1,12 +1,19 @@
 const express = require('express');
 
-const { Article } = require('../model');
+const { submitedArticleModel } = require('../model/submitArticle');
 
 const articleRouter = express.Router();
 
+articleRouter.get('/', async (request, response) => {
+  const submitedArticle = await submitedArticleModel.find();
+  return response.status(200).json(submitedArticle).end();
+});
+
 articleRouter.post('/', async (request, response) => {
   const { body } = request;
-  const result = await Article.insertMany([body]);
+  const result = await submitedArticleModel.insertMany([body]).catch(() => {
+    response.status(400).json('Invalid data');
+  });
   return response.json(result);
 });
 
